82a85337f0a2b27c591db1688644c550b7435a9e
fnparr <fnparr@gmail.com>
2022-08-17 23:05:43 -0400

    the PAckage shiny app now runs with data from inst/extdata

---
+++ R/runShinyDemo.R
@@ -0,0 +1,14 @@
+# ****************
+# runDemo.R    - package function to run a Shiny demo app
+#' runDaDFiR3demo()  Launches a shiny demo application
+#' 
+#' Launches a browser controlled application plotting the provided sample 
+#' reference rate data in a display window. You can chose between plots 
+#' of  5yTreasuryBill_fallingRates, 5yTreasuryBill_recoveringRates, etc 
+#' 
+#' @export
+#' @importFrom shiny runApp
+runDaDFiR3demo <- function (){
+  appDir <- system.file("shiny-examples","DaDFiR3-Demo", package = "FEMSdevPkg")
+  shiny::runApp(appDir)
+}
