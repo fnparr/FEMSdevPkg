f55fd3aa72d6301f0a3f3643daa9ea9e52e21f30
fnparr <fnparr@gmail.com>
2024-02-10 13:26:32 -0500

    Updated version of accountTestMain - experimenting with data.tree and a relevant simple example dataset. We can (1) index into the accounts (2) assign a unique NodeID to each node (4) set attributes to leaf Nodes. Have NOT yet found how to: (4) build an index mapping CIDs to the owning leaf nodeID or pathName (5) get aggregate to work on vectors of leaf values.

--- accountTestMain.R
+++ accountTestMain.R
@@ -1,6 +1,6 @@
 library(data.tree)
 library(yaml)
-# Test 0.1
+# Test 0.1   show tree structure with contractid list at leaf  nodes 
 rm(list=ls())
 yaml2 <- "
 name: Powerplant Accounts
@@ -30,3 +30,50 @@ print(accnts2Node)
 accnts2Node$Assets$Current$actusCIDs
 accnts2Node$Assets$ShortTerm$actusCIDs
 accnts2Node$Assets$ShortTerm$formulaCIDs
+# Experiment 0.2  (test failed)  build index of CIDs to owning leaf account
+# tree indexing using climb and account name segments 
+accnts2Node$Climb(name="Assets")$Climb(name="ShortTerm")$path
+# Need to generate a unique nodeID for each leaf and non leaf node for contract
+# report aggregation 
+accnts2Node$Set(nodeID = 1:accnts2Node$totalCount)
+# convert to data frame - row per leaf node  with list - full path, last pos
+treemap2 <- ToDataFrameTable(accnts2Node,"path","nodeID","actusCIDs")
+# filter out any rows with no defined contracts 
+tmcids <- subset(treemap2, ! is.na(actusCIDs))
+# build list of contractID to leaf maps 
+accountIDmap <- function (tmcids) {
+   map <- list() 
+  lapply(1:nrow(tmcids), function(i){
+    for (j in 1:length(tmcids$actusCIDs[i])) {
+      item <- list(cid= tmcids$actusCIDs[j], accountID= tmcids$nodeID[i])
+      map[[length(map)+1]] <- item 
+    }
+  })
+  return(map)
+}
+# => problem is that data.frame merges vector of CIDs into a string
+# Test 0.3 Show that the vector of CIDs at each leaf account is indexable 
+accnts2Node
+length(accnts2Node$Assets$Current$actusCIDs)
+accnts2Node$Assets$Current$actusCIDs[2]
+
+# Test 0.4 Test aggregation of numeric vectors ( income reports) to tree 
+incNull <- c(rep1= 0.0, rep2= 0.0, rep3 = 0.0)
+incCurrent <- c(rep1= 1.0, rep2= 1.1, rep3=1.2)
+incShortTerm <- c(rep1= 10.0, rep2= 10.1, rep3=10.2)
+incLongTerm <- c(rep1 = 0.5, rep2= 0.8, rep3 = 20.9 )
+incDebt <- c(rep1= - 0.1, rep2 = - 0.5 , rep3 = - 0.5)
+
+accnts2Node$Assets$Current$income <- incCurrent[1]
+accnts2Node$Assets$ShortTerm$income <- incShortTerm[1]
+accnts2Node$Assets$Longterm$income <- incLongTerm[1]
+accnts2Node$Liabilities$Debt$income <- incDebt[1]
+accnts2Node$Liabilities$Equity$income <- incNull[1]
+accnts2Node$Operations$Revenues$income <- incNull[1]
+accnts2Node$Operations$Expenses$income <- incNull[1]
+
+# now write the aggregate function which propagates to all accounts in tree
+# needs to be able to ignore na values in aggregation 
+Aggregate( node=accnts2Node, attribute = "income",  aggFun = sum)
+accnts2Node$attributesAll
+
