dda6bade9edb0614a7a3418a8ad4a2b5b3db5d2b
fnparr <fnparr@gmail.com>
2024-03-27 18:32:56 -0400

    financialModel tests now maps end to end steps for analysis of a portfolio using specific risk scenario in a financial model context. But some stepa missing

--- test/financialModelTests.R
+++ test/financialModelTests.R
@@ -61,7 +61,7 @@ fm <- FinancialModel(fmID = fmID,fmDescr = fmDescr, entprID = entprID,
                      curr = currency, serverURL = serverURL )
 class(fm)
 
-# test 3.0
+# Test 3.0
 # next Step is to run a ContractAnalysis as is with the attributes of this fm 
 # Use contents of unitTestMain.R test 3.0 
 fm$serverURL
@@ -87,42 +87,62 @@ cfla <- ContractAnalysis( analysisID = fm$financialModelID,
                           scenario =   scenario,
                           actusServerURL = fm$serverURL,
                           timeline = fm$timeline)
-#
+
+# check valid ContractAnalysis object created
+class(cfla)
+
+# Test 3.1:  
 # simulate the portfolio, this scenario; results go into cfla$cashflowEventsLoL
-msg <- generateEvents(cntan= cfla)
-# check whether simulation POST (REST) call 
-msg
+logMsgs1 <- generateEvents(cntan= cfla)
+logMsgs1
+
 # check whether each contract was successfully simulated 
 unlist(lapply(cfla$cashflowEventsLoL,function(x){return(x$status)}))
 
 # Test 3.1 Group events by (Timeline) periods 
 logMsgs2  <- events2dfByPeriod(cfla = cfla)
 logMsgs2
+
 # check on dataframe contents 
 cfla$cashflowEventsByPeriod[1:10,]
 rows <-nrow(cfla$cashflowEventsByPeriod)
 cfla$cashflowEventsByPeriod[(rows-10):rows,]
 
+# Test 3.2 Generate liquidity vectors But undo cumsum if there 
+logMsgs3  <- liquidityByPeriod2vec(cfla= cfla)
+logMsgs3
 
-# ******** Got to Here FNP 26 March 2024 
-# Test 0.2 
-rm(list=ls())
-library(yaml)
-yaml<- "
-name: Powerplant Accounts
-Assets:
-  Current:
-  ShortTerm:
-  Longterm:
-Liabilities:
-  Debt:
-  Equity:
-Operations:
-  Revenues:
-  Expenses:
-"
-accountsList <- yaml.load(yaml)
-accountsNode <- as.Node(accountsList, interpretNullAsList = TRUE)
-fmID <- "fm001"
-fmDescr <- "Text describing the model"
-entprID <-"Powerplant"
+# check
+typeof(cfla$contractLiquidityVectors)
+length(cfla$contractLiquidityVectors)
+cfla$contractLiquidityVectors$pam001
+unlist(cfla$contractLiquidityVectors)
+# Some of the liquidity vectors are missing elements 
+# Why are there6 periods - because 3 reports and 6 periods !
+cfla$timeline$periodCount
+# Contract liquidity vectors is ALL periods , not rfixed length, periodID
+
+# Test 3.3 generate income reports
+logMsgs5  <- eventsdf2incomeReports(cfla= cfla)
+logMsgs5
+
+unlist(cfla$incomeReports)
+
+logMsgs6 <- nominalValueReports(cntan=cfla)
+logMsgs6
+unlist(cfla$nominalValueReports)
+cfla$nominalValueReports[[1]]
+cfla$nominalValueReports[[3]]
+typeof(cfla$nominalValueReports[[1]]$nvreps)
+# somehow the values have been converted to character 
+# == need to fix this 
+as.numeric(cfla$nominalValueReports[[1]]$nvreps)
+fixNvrs <- function(cnvrsList){
+   nnvrs <- lapply(cnvrsList, function(nvr){
+     return(list(cid=nvr$cid,nvreps = as.numeric(nvr$nvreps)))
+   })
+}
+nnvrs <- fixNvrs(cfla$nominalValueReports)
+
+# Test 5:  can we display aggregated nominal Values in the accounts tree 
+
