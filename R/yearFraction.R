b93b0db80b7882a201b712c0ce95337cd9b0f4e9
giantomm <127210723+giantomm@users.noreply.github.com>
2024-11-20 16:07:10 +0100

    yearFraction fix due to unavailability of fmdates package

--- R/yearFraction.R
+++ R/yearFraction.R
@@ -1,46 +1,46 @@
-# YieldCurve.R  FEMSdevPkg code by Francis Parr Nov 2023 
+# YieldCurve.R  FEMSdevPkg code by Francis Parr Nov 2023
 #   updates earlier git wbreymann/FEMS code by Henriette-Elise Breymann
 # Licensing and Copyright notices to be added  XXXX
 # ************************************************************************
-# yearFraction(Tfrom, Tto, yfDayCountConvention) 
-#    -- NONexported wrapper for functions lubridate::ymd() and 
-# ************************************************************************    
+# yearFraction(Tfrom, Tto, yfDayCountConvention)
+#    -- NONexported wrapper for functions lubridate::ymd() and
+# ************************************************************************
 setGeneric(name = "yearFraction",
            def = function(Tfrom,Tto,yfdcc){
              standardGeneric("yearFraction")
            })
 
 # ************************************************************************
-#' yearFraction(Tfrom, Tto, yfDayCountConvention) 
-#'    -- NONexported wrapper for functions lubridate::ymd() and 
-#        fmdates::year_frac() 
+#' yearFraction(Tfrom, Tto, yfDayCountConvention)
+#'    -- NONexported wrapper for functions lubridate::ymd() and
+#        RQuantLib::yearFraction()
 #'
 #'   Function yearFraction(character, character, character ) takes as
-#'   input: (1) a yyyy-mm-dd start date of the interval, (2) a yyyy-mm-dd 
-#'   end date for the interval and (3) a year_frac supported dayCountConvention
-#'   string.  It returns the length of the interval as a fractional number of 
+#'   input: (1) a yyyy-mm-dd start date of the interval, (2) a yyyy-mm-dd
+#'   end date for the interval and (3) a RQuantLib::yearFraction supported dayCountConvention
+#'   string.  It returns the length of the interval as a fractional number of
 #'   years.  If Dto is earlier (<) Dfrom a negative fractional number of years
-#'   is returned. 
-#'    
-#'   Function yearFraction(Tfrom,Tto, yfdcc) depends on and uses external 
-#'   library functions: (1) lubridate::ymd() which converts a yyyy-mm-dd 
+#'   is returned.
+#'
+#'   Function yearFraction(Tfrom,Tto, yfdcc) depends on and uses external
+#'   library functions: (1) lubridate::ymd() which converts a yyyy-mm-dd
 #'   character string date into a Date value and
-#'    (2) fmdates::year_frac(Dfrom, Dto, yfdcc) which requires Dfrom and Dto 
-#'    to be Date values and yfdcc a year_frac dayCountConvention string 
+#'    (2) RQuantLib::yearFraction(startDates, endDates, dayCounters) which requires startDates and endDates
+#'    to be Date vectors and dayCounters a vector of numeric dayCountConvention values
+#'    (see https://search.r-project.org/CRAN/refmans/RQuantLib/html/Enum.html).
 #'
 #' @param Tfrom      character  start date of interval in yyyy-mm-dd form
-#' @param Tto.       character  end date of internal in yyyy-mm-dd form 
-#' @param yfdcc      character  year_frac supported dayCountConvention value 
-#' @return       Length of time interval in fractional number of years 
+#' @param Tto.       character  end date of internal in yyyy-mm-dd form
+#' @param yfdcc      character  RQuantLib::yearFraction supported dayCountConvention value
+#' @return       Length of time interval in fractional number of years
 #' @importFrom lubridate ymd
-#' @importFrom fmdates year_frac
+#' @importFrom RQuantLib yearFraction
 
-setMethod(f = "yearFraction", signature = c("character", "character", "character"),
+setMethod(f = "yearFraction", signature = c("character", "character", "numeric"),
           definition = function(Tfrom, Tto, yfdcc){
-            
             # calculate year fraction
-            frac <- fmdates::year_frac(lubridate::ymd(Tfrom), 
-                                       lubridate::ymd(Tto), 
-                                       yfdcc)
+            frac <- RQuantLib::yearFraction(lubridate::ymd(Tfrom),
+                                            lubridate::ymd(Tto),
+                                            dayCounters = yfdcc)
             return(frac)
           })
